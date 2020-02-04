6263184767283b3ddd91f79e55278430b1e93ec3
daibhin <david@monaru.io>
2020-02-04 18:18:55 +0000

    Resources panel

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,52 +20,80 @@ class BlogIndex extends React.Component {
         description={description}
       >
         <SEO title="All posts" />
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <Link style={{ boxShadow: "none" }} to={node.fields.slug}>
-                <article class="article-container" key={node.fields.slug}>
-                  <h3
-                    style={{
-                      marginTop: "8px",
-                      marginBottom: "18px",
-                      textAlign: "center",
-                      fontSize: "28px",
-                    }}
-                  >
-                    {title}
-                  </h3>
-                  <div class="author mt_12">
-                    <img class="profile-icon" src={profileImage} />
-                    <div
-                      style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        marginLeft: "8px",
-                      }}
-                    >
-                      <span class="small-text light-text">Patrick</span>
-                      <span
-                        style={{ marginTop: "-3px" }}
-                        class="tiny-text grey-text"
+
+        <div class="home-container">
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              flex: 1,
+            }}
+          >
+            {posts.map(({ node }) => {
+              const title = node.frontmatter.title || node.fields.slug
+              return (
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  <Link style={{ boxShadow: "none" }} to={node.fields.slug}>
+                    <article class="article-container" key={node.fields.slug}>
+                      <h3
+                        style={{
+                          marginTop: "8px",
+                          marginBottom: "18px",
+                          textAlign: "center",
+                          fontSize: "28px",
+                        }}
                       >
-                        7 minute read
-                      </span>
-                    </div>
-                  </div>
-                </article>
-              </Link>
-            </div>
-          )
-        })}
+                        {title}
+                      </h3>
+                      <div class="author mt_12">
+                        <img class="profile-icon" src={profileImage} />
+                        <div
+                          style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            marginLeft: "8px",
+                          }}
+                        >
+                          <span class="small-text light-text">Patrick</span>
+                          <span
+                            style={{ marginTop: "-3px" }}
+                            class="tiny-text grey-text"
+                          >
+                            7 minute read
+                          </span>
+                        </div>
+                      </div>
+                    </article>
+                  </Link>
+                </div>
+              )
+            })}
+          </div>
+
+          <div
+            style={{
+              margin: "10px",
+              display: "flex",
+              flexDirection: "column",
+              minWidth: "250px",
+            }}
+          >
+            <u>Resources</u>
+
+            <Link to="x4y">
+              X4Y startup idea generator
+            </Link>
+          </div>
+
+        </div>
+          
       </Layout>
     )
   }
